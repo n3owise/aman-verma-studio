7a697fb57a1b7a24732649dd41f0e4dd109ed8fc
n3owise <aman194043@ihmchandigarh.org>
2026-09-04 13:50:00 +0530

    chore: remove sanity-hosted appId

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -6,7 +6,6 @@ export default defineCliConfig({
     dataset: 'production',
   },
   deployment: {
-    appId: 'u5egknpljqhq42sulby40510',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
